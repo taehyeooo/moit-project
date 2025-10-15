349b3b88631ea3919aa7cacfc784835af5d19fd6
cjj702 <cjj702m@gmail.com>
2025-10-15 15:01:34 +0900

    이미지 업로드 찐막

--- html/backend/models/User.js
+++ html/backend/models/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
       type: String, required: true, trim: true,
       lowercase: true, minlength: 4, unique: true
     },
-    password: { type: String, required: true, select: false },
+    password_hash: { type: String, required: true, select: false },
     name: { type: String, required: true, trim: true }, // <<< 추가: 이름
     nickname: { type: String, required: true, trim: true, unique: true }, // <<< 추가: 닉네임
     email: { type: String, required: true, trim: true, unique: true }, // <<< 추가: 이메일
